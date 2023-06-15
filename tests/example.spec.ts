d8b5ae6abe5b8ef5ad65c2278eefd2265345b74d
杵島健 / KISHIMA，TAKESHI <takeshi.kishima.zk@ps.hitachi-solutions.com>
2023-06-15 15:10:16 +0900

    スクリーンショットとってみた

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -9,6 +9,12 @@ test("login", async ({ page }) => {
   await page.goto("https://localhost:8443/jenkins/login");
   await page.locator("#j_username").fill("admin");
   await page.locator("body > div > div > form > div:nth-child(2) > input").fill("admin");
+  await page.screenshot({ path: "A.png" });
   await page.locator("body > div > div > form > div.submit > button").click();
+  // URLのテスト。スクリーンショット取れないのかね
   await expect(page).toHaveURL(/jenkins/);
+  await page.screenshot({ path: "B.png" });
+  // テーブルを指定した状態でスクリーンショットしてみた。(idで指定してidの確認は気にしないでください…)
+  await expect(page.locator("#projectstatus")).toHaveId("projectstatus");
+  await page.screenshot({ path: "C.png" });
 });
